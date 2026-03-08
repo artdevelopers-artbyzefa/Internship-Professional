e547ca87f8b3b75ed2e7356b131b1f70565740c7
Huzaifa Safdar <chdps257@gmail.com>
2026-03-09 01:20:07 +0500

    Update CORS settings for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -25,7 +25,10 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+        "http://localhost:5173",
+        "https://internship-professional-ie1e.vercel.app"
+    ],
     credentials: true
 }));
 app.use(cookieParser());
